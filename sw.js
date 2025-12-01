ac6f4caa1c18da4093bf8e6e08b97a2ce72d376f
DevDW <dweminem@gmail.com>
2025-12-01 20:04:40 -0300

    feat: Integrate Monetag ads
    Adds Monetag ad scripts to index.html and updates the zoneId in sw.js to integrate advertising into the Filmes Online application.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10264432
+    "zoneId": 10264458
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
