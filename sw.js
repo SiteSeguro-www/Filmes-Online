320c128f80dc52ff106ceb443087831cf5f49872
DevDW <dweminem@gmail.com>
2025-12-18 09:25:47 -0300

    feat: Update Monetag ad scripts and service worker
    Replaces the older Monetag ad script with a new multi-tag script and updates the service worker to use the correct zone ID. This ensures proper ad delivery and functionality.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10264458
+    "zoneId": 10344219
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
